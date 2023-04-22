4255d90a4cc1469042e6bf10604a6f9f37ad9c3b
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2023-04-22 20:04:53 +0700

    adding dynamic energy treshold

--- recog.py
+++ recog.py
@@ -3,17 +3,17 @@ import subprocess
 
 fileNumber = input("Masukkan nomor file: ")
 
-audionameMp3 = "track"+fileNumber+".mp3"
-audionameWav = "track"+fileNumber+".wav"
+audionameMp3 = "Track "+fileNumber+".mp3"
+audionameWav = "Track "+fileNumber+".wav"
 subprocess.call(['ffmpeg', '-i', audionameMp3, audionameWav])
-#subprocess.call(['ffmpeg', '-i', "track11.mp3", "track11.mav"])
 
 txtname = "track"+fileNumber+".txt"
 
 r = sr.Recognizer()
+r.dynamic_energy_threshold = True
 with sr.AudioFile(audionameWav) as source:
     audio_data = r.record(source)
-    text = r.recognize_google(audio_data,language='id-ID')
+    text = r.recognize_google(audio_data, language='id-ID')
     print(text)
 
 with open(txtname, 'w') as f:
